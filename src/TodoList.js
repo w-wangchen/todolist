4ce0a6502af37b27b6cff0e8c45635cda024818d
wangchn86@163.com <wangchn86@163.com>
2020-07-24 07:37:15 +0800

    响应式设计思想和事件绑定

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,16 +2,25 @@
  * @作者 wangchn86@163.com
  * @创建时间 2020/7/22 12:49 上午
  */
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 
-class TodoList extends Component{
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: 'hello world',
+      list: [],
+    }
+  }
 
   render() {
 
     return (
       <Fragment>
         <div>
-          <input/>
+          <input value={ this.state.inputValue }
+                 onChange={ this.handleInputChange.bind(this) }/>
           <button>提交</button>
         </div>
         <ul>
@@ -20,6 +29,14 @@ class TodoList extends Component{
         </ul>
       </Fragment>
     )
+  }
+
+  handleInputChange(e) {
+    // console.log(e.target)// input dom
+    // console.log(this)
+    this.setState({
+      inputValue: e.target.value,
+    })
   }
 }
 

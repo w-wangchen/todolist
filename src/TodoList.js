25f9091a17a8d86f0cd78147ff9dd62254309a16
wangchn86@163.com <wangchn86@163.com>
2020-07-24 08:26:57 +0800

    jsx 语法补充:注释、不转译、样式引入、增加选中范围

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,6 +3,7 @@
  * @创建时间 2020/7/22 12:49 上午
  */
 import React, { Component, Fragment } from 'react'
+import './style.css'
 
 class TodoList extends Component {
 
@@ -18,9 +19,16 @@ class TodoList extends Component {
 
     return (
       <Fragment>
+        {
+          // 单行注释
+        }
         <div>
-          <input value={ this.state.inputValue }
-                 onChange={ this.handleInputChange.bind(this) }/>
+          <label htmlFor={ 'insertArea' }>输入内容：</label>
+          <input id={ 'insertArea' }
+                 value={ this.state.inputValue }
+                 onChange={ this.handleInputChange.bind(this) }
+                 className={ 'input' }
+          />
           <button onClick={ this.handleBtnClick.bind(this) }>提交</button>
         </div>
         <ul>
@@ -28,7 +36,9 @@ class TodoList extends Component {
             this.state.list.map((item, index) => {
               return (
                 <li key={ index }
-                    onClick={ this.handleItemDelete.bind(this, index) }>{ item }</li>
+                    onClick={ this.handleItemDelete.bind(this, index) }
+                    dangerouslySetInnerHTML={ { __html: item } }
+                ></li>
               )
             })
           }
